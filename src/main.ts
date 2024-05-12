a142ec642e7750012049ac41f431699795c8529c
Modou Aicha Diop <modouaichadiop@gmail.com>
2024-05-12 18:54:26 +0000

    Add dotenv package and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: process.env.URL_CORS_ACCESS,
+    credentials: true,
+  });
   await app.listen(3001);
 }
 bootstrap();
